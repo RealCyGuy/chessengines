d3908304a389c6658f792baca73f9a5c34d4de0f
Cyrus Yip <cyruscmyip1@gmail.com>
2022-06-25 18:17:09 -0700

    fix engine list source lines

--- scripts/showengines.py
+++ scripts/showengines.py
@@ -8,7 +8,7 @@ for x, y in Engines.__dict__.items():
         f = getattr(Engines, x)
         sourcelines = inspect.getsourcelines(f)
         lines.append(
-            f"[`{x}`](https://github.com/RealCyGuy/chessengines/blob/main/engines.py#L{sourcelines[1]}-{len(sourcelines[0]) + sourcelines[1]}): {f.__doc__}"
+            f"[`{x}`](https://github.com/RealCyGuy/chessengines/blob/main/engines.py#L{sourcelines[1]}-L{len(sourcelines[0]) + sourcelines[1]}): {f.__doc__}"
         )
 
 print("  \n".join(lines))
